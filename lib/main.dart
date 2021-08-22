e5d1dd6ddab4be890fb07a9ec6e0be9cd5833e4c
Arjun Puri <arjunpuri@arjunpuri.me>
2021-08-23 02:08:25 +0530

    completed challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: eightBall(),
       ),
     );
+
+class eightBall extends StatefulWidget {
+  const eightBall({Key? key}) : super(key: key);
+
+  @override
+  _eightBallState createState() => _eightBallState();
+}
+
+class _eightBallState extends State<eightBall> {
+  int answer = 1;
+
+  void rollAnswer() {
+    setState(() {
+      answer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[800],
+        // centerTitle: false,
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: TextButton(
+          child: Image.asset('images/ball$answer.png'),
+          onPressed: rollAnswer,
+        ),
+      ),
+    );
+  }
+}
